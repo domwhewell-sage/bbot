5fee4323841a9113aa1af3a9b579c1ddcd9e7d4e
GitHub <noreply@github.com>
2024-06-03 10:14:50 +0000

    Update nuclei

--- bbot/modules/deadly/nuclei.py
+++ bbot/modules/deadly/nuclei.py
@@ -15,7 +15,7 @@ class nuclei(BaseModule):
     }
 
     options = {
-        "version": "3.2.0",
+        "version": "3.2.8",
         "tags": "",
         "templates": "",
         "severity": "",
